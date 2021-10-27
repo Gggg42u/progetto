49bb825a2d91b367709e46f4650ce1b5887abcf8
Georgia Togni <80409590+Gggg42u@users.noreply.github.com>
2021-10-27 18:27:35 +0200

    Update file1.c

--- file1.c
+++ file1.c
@@ -6,33 +6,56 @@
 // donnees meteo
 
 
-double affluantCOD_TKN(double eau, double COD_TKN) //eau (m^3/min), COD/TKN (g/m^3), T (°C) 
-{																   
-	double MO_N = eau * COD_TKN;		//MO/N (1/min)		
-	return MO_N;
+//Calcul des grammes par minute de TKN dans l'affluant
+
+double TKN_affluant(double Q, double TKN_in){ 			//Q : débit [m^3/min], TKN [g/m^3], T [°C] 
+												   
+	double TKN_aff = Q * TKN_in;						//TKN_aff [g/min]		
+	return TKN_aff;
 }
 
-double croissance_hetero(double t, double COD)
-{
-	double µmax_h = 6.0 * exp(0.0693*t-1.386);
+//Calcul des grammes par minute de COD dans l'affluant
+
+double COD_affluant(double Q, double COD_in){ 			//Q : débit [m^3/min], COD [g/m^3], T [°C] 
+										   
+	double COD_aff = Q * COD_in;						//COD_aff [g/min]		
+	return COD_aff;
+}
+
+//Calcul du taux de croissance des bactéries héterotrophes 
+
+double croissance_hetero(double t, double COD_aff){ 	// t [jours]
+
+	double µmax_h = 6.0 * exp(0.0693*t-1.386); ; 		// µmax_h [1/jour]
 	double ks_h = 20;
-	double µ_h = µmax_h * (COD/(ks_h + COD));
-	double µ_min_h = pow((µ_h + 1),1/(24*60))-1;
-	return µ_min_h;
+	double µ_h = µmax_h * (COD_aff/(ks_h + COD_aff)); ; // µ_h [1/jour]
+	double µ_min_h = pow((µ_h + 1),1.0/(24*60))-1;
+	return µ_min_h; 									// µ_min_h [1/min]
 } 
 
 //Calcul du taux de croissance des bactéries autotrophes 
 
-double croissance_auto(double t, double TKN) {
-	double µmax_a = 0.85*exp(0.106*(t-20));
+double croissance_auto(double t, double TKN) { 			// t [jours]
+	
+	double µmax_a = 0.85*exp(0.106*(t-20));				// µmax_a [1/jour]
 	double ks_a = pow(10, 0.051*t - 1.158);
-	double µ_a = µmax_a * exp(0.098*(t-15)) * TKN/(ks_a + TKN);
-	double µ_min_a = pow((µ_a + 1),1/(24*60))-1;
-	return µ_min_a;
+	double µ_a = µmax_a * exp(0.098*(t-15)) * TKN/(ks_a + TKN);		// µ_a [1/jour]
+	double µ_min_a = pow((µ_a + 1),1.0/(24*60))-1;
+	return µ_min_a;										// µ_min_h [1/min]
 }
 	
-int main(int argc, char ** argv)
-{
-    printf("µ_min_h: %.25f\n", croissance_hetero(10,3));
-    printf("µ_min_a: %f", croissance_auto(10, 3));
+int main(int argc, char ** argv){
+	
+	double TKN_aff = TKN_affluant(5, 30);
+	printf("Quantité de TKN: %.2f [g/min]\n", TKN_aff);
+	
+	double COD_aff = COD_affluant(340, 30);
+	printf("Quantité de COD: %.2f [g/min]\n", COD_aff);
+	
+	double µ_min_h = croissance_hetero(100, 340);
+	printf("Taux de croissance par minute des bact. hétérotrophes: %.2f [1/min]\n", µ_min_h);
+	
+	double µ_min_a = croissance_auto(100, 30);
+	printf("Taux de croissance par minute des bact. autotrophes: %.2f [1/min]\n", µ_min_a);
+	
 }
