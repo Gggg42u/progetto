d02bc4218c31dc112c770d2af223e5384512777e
AlessandroValentini3 <alessandro.valentini33@gmail.com>
2021-10-27 15:00:47 +0200

    lezzo

--- file1.c
+++ file1.c
@@ -14,16 +14,14 @@ double affluantCOD_TKN(double eau, double COD_TKN) //eau (m^3/min), COD/TKN (g/m
 
 double croissance_hetero(double t, double COD)
 {
-	double exp_term = exp(0.0693*t-1.386);
-	double mu_max = 6.0 * exp_term;
+	double µ_max = 6.0 * exp(0.0693*t-1.386);
 	double k_s = 20;
-	double mu = mu_max * (COD/(k_s +COD));
-	double mu_min = pow((mu + 1),1/(24*60))-1;
-	return mu_min;
+	double µ = µ_max * (COD/(k_s + COD));
+	double µ_min = pow((µ + 1),1/(24*60))-1;
+	return µ_min;
 } 
 
 int main(int argc, char ** argv)
 {
-    printf("%f", affluantCOD_TKN(10,20));
-    printf("%f", croissance_hetero(1,1));
+    printf("%lf", croissance_hetero(5,10));
 }
