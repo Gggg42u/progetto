17a69c5fa8f2f0c71a6fcdf6afb2f95e0daf41be
Georgia Togni <80409590+Gggg42u@users.noreply.github.com>
2021-10-27 16:42:15 +0200

    Update file1.c

--- file1.c
+++ file1.c
@@ -14,11 +14,11 @@ double affluantCOD_TKN(double eau, double COD_TKN) //eau (m^3/min), COD/TKN (g/m
 
 double croissance_hetero(double t, double COD)
 {
-	double µmax_e = 6.0 * exp(0.0693*t-1.386);
-	double ks_e = 20;
-	double µ_e = µmax_e * (COD/(ks_e + COD));
-	double µ_min_e = pow((µ_e + 1),1/(24*60))-1;
-	return µ_min_e;
+	double µmax_h = 6.0 * exp(0.0693*t-1.386);
+	double ks_h = 20;
+	double µ_h = µmax_h * (COD/(ks_h + COD));
+	double µ_min_h = pow((µ_h + 1),1/(24*60))-1;
+	return µ_min_h;
 } 
 
 //Calcul du taux de croissance des bactéries autotrophes 
@@ -27,11 +27,12 @@ double croissance_auto(double t, double TKN) {
 	double µmax_a = 0.85*exp(0.106*(t-20));
 	double ks_a = pow(10, 0.051*t - 1.158);
 	double µ_a = µmax_a * exp(0.098*(t-15)) * TKN/(ks_a + TKN);
-	return µ_a;
+	double µ_min_a = pow((µ_a + 1),1/(24*60))-1;
+	return µ_min_a;
 }
 	
 int main(int argc, char ** argv)
 {
-    printf("µ_e: %f\n", croissance_hetero(10,3));
-    printf("µ_a: %f", croissance_auto(10, 3));
+    printf("µ_min_h: %.25f\n", croissance_hetero(10,3));
+    printf("µ_min_a: %f", croissance_auto(10, 3));
 }
