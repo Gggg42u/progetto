6bdc4543ac602aaa5112cde16718562689cb5b1e
GeorgiaTogni <georgia@dilmun.info>
2021-10-27 15:10:11 +0200

    dio

--- file1.c
+++ file1.c
@@ -28,7 +28,7 @@ double croissance_auto(double t, double TKN) {
 	double µmax_a = 0.85*exp(0.106*(t-20));
 	double ks_a = pow(10, 0.051*t - 1.158);
 	double µ_a = µmax_a * exp(0.098(t-15)) * TKN/(ks + TKN);
-	double 
+	double greco = 1;
 	
 int main(int argc, char ** argv)
 {
